715ba3ed1c297d37ecb50986f405d92374f6cea5
shrutitrimukhe <112937968+shrutitrimukhe@users.noreply.github.com>
2022-09-27 15:04:21 +0530

    Add files via upload

---
+++ Cricketer.java
@@ -0,0 +1,14 @@
+package casting;
+
+public class Cricketer extends Player {
+
+	void stadium()
+	{
+		System.out.println("playing for cricket");
+	}
+	
+	void bat()
+	{
+		System.out.println("For batting");
+	}
+}
