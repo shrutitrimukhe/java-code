d590f7aff7b2a2650d7321f10210ab6d8c7c7b29
shrutitrimukhe <112937968+shrutitrimukhe@users.noreply.github.com>
2022-09-27 15:03:46 +0530

    Add files via upload

---
+++ Footballer.java
@@ -0,0 +1,16 @@
+package casting;
+
+public class Footballer extends Player{
+	
+	void stadium()
+	{
+		System.out.println("Playing for football");
+	}
+	
+	void goal()
+	{
+		System.out.println("He is a goalkeeper.");
+	}
+	
+
+}
