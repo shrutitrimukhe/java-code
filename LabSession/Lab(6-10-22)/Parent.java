2e3d3912ed0454cc06d9cb2bba4fec85ccd17e82
shrutitrimukhe <112937968+shrutitrimukhe@users.noreply.github.com>
2022-10-06 13:21:37 +0530

    Update Parent.java

--- LabSession/Lab(6-10-22)/Parent.java
+++ LabSession/Lab(6-10-22)/Parent.java
@@ -1,3 +1,10 @@
+/*create a class parent with default constructor and void sing method.print something inside the constructor and method.create a child class which will inherit
+ * the property of parent class and has methods void speak and void dance.print something inside the two methods.create another class inheritance and object of class and
+ * call all the methods.also mentioned the types of inheritance on the top of program.
+ * Name:shruti
+ * Date:6-10-22
+ */
+
 package lab;
 //Creating parent class
 public class Parent{
