5077ba806378f41893c6ecd0312f19a9b81a15a1
shrutitrimukhe <112937968+shrutitrimukhe@users.noreply.github.com>
2022-10-12 12:00:42 +0530

    Update DoWhileLoop1.java

--- hands on/DoWhileLoop1.java
+++ hands on/DoWhileLoop1.java
@@ -10,7 +10,7 @@ public class DoWhileLoop1 {
 
 	public static void main(String[] args) {
 		
-		int n = 0,rev=0;
+		int rev=0;
 		//Taking input from the user
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a number:  ");
@@ -19,9 +19,9 @@ public class DoWhileLoop1 {
 		//condition 
 		do
 		{
-			int a1 = n%10;
+			int a1 = num%10;
 			rev = rev*10 + a1;
-			n = n/10;
+			num = num/10;
 		}
 		
 		//Test condition.
