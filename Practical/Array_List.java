702a460166a90ad9fb3ae60196a014bdb6a6373d
shrutitrimukhe <112937968+shrutitrimukhe@users.noreply.github.com>
2022-10-22 14:09:59 +0530

    Add files via upload

---
+++ Practical/Array_List.java
@@ -0,0 +1,21 @@
+/*
+ * Name:Shruti Trimukhe
+ */
+package Practice;
+
+import java.util.ArrayList;
+
+public class Array_List {
+
+	public static void main(String[] args) {
+
+		ArrayList<String>cars=new ArrayList<String>();
+		cars.add("Volvo");
+		cars.add("BMW");
+		cars.add("Ford");
+		cars.add("Mazda");
+		 System.out.println(cars);
+		
+	}
+
+}
