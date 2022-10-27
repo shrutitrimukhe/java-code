a464ceab6eeba838f8e9266be392251db10c61b4
shrutitrimukhe <112937968+shrutitrimukhe@users.noreply.github.com>
2022-10-27 13:36:48 +0530

    Add files via upload

---
+++ LabSession/Lab 27-10-2022/Happy_Diwali.java
@@ -0,0 +1,23 @@
+/*Q.WAP to print HAPPY DIWALI using any loop.
+ * Name:Shruti Trimukhe
+ * Date:27-10-2022
+ */
+package Practice;
+
+public class Happy_Diwali {
+   //Using Main Method
+	public static void main(String[] args) {
+		//declaring the string 
+		String s= "HAPPY DIWALI";
+		
+		char[] arr=s.toCharArray();
+        //Using for loop
+		for (int i=0;i<s.length();i++)
+		{
+        //Printing the string
+		System.out.println(s);
+		////break statement to exit the loop.
+		break;
+		}
+   }
+}
